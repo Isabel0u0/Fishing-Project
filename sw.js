b92798a1245be142c523d078129ec442349e1a7a
Isabel0u0 <anaisa2009.08@gmail.com>
2026-09-16 23:02:17 -0600

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,46 +1,49 @@
-const CACHE_NAME = "phishguard-v3";
+const CACHE_NAME = "fishing-v4";
 
 const ARCHIVOS = [
-    "./",
-    "./index.html",
-    "./styles.css",
-    "./manifest.json",
-    "./icon-192.png",
-    "./icon-512.png"
+"./",
+"./index.html",
+"./styles.css",
+"./manifest.json",
+"./icon-192.png",
+"./icon-512.png"
 ];
 
 self.addEventListener("install", event => {
-    self.skipWaiting();
+self.skipWaiting();
 
-    event.waitUntil(
-        caches.open(CACHE_NAME).then(cache => {
-            return cache.addAll(ARCHIVOS);
-        })
-    );
+```
+event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+        return cache.addAll(ARCHIVOS);
+    })
+);
+```
+
 });
 
 self.addEventListener("activate", event => {
-    event.waitUntil(
-        caches.keys().then(keys =>
-            Promise.all(
-                keys.map(key => {
-                    if (key !== CACHE_NAME) {
-                        return caches.delete(key);
-                    }
-                })
-            )
-        ).then(() => self.clients.claim())
-    );
+event.waitUntil(
+caches.keys().then(keys =>
+Promise.all(
+keys.map(key => {
+if (key !== CACHE_NAME) {
+return caches.delete(key);
+}
+})
+)
+).then(() => self.clients.claim())
+);
 });
 
 self.addEventListener("fetch", event => {
-    event.respondWith(
-        fetch(event.request)
-            .then(response => {
-                return response;
-            })
-            .catch(() => {
-                return caches.match(event.request);
-            })
-    );
+event.respondWith(
+fetch(event.request)
+.then(response => {
+return response;
+})
+.catch(() => {
+return caches.match(event.request);
+})
+);
 });
