835080ab68f74f8a44aeb8346d54ed089f398af5
Isabel0u0 <anaisa2009.08@gmail.com>
2026-09-17 00:42:41 -0600

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,42 +1,24 @@
-const CACHE_NAME = "fishing-app-v1";
+const CACHE_NAME = "phishguard-v1";
 
-const FILES = [
-  "./",
-  "./index.html",
-  "./styles.css",
-  "./manifest.json",
-  "./icon-192.png",
-  "./icon-512.png"
+const ARCHIVOS = [
+    "./",
+    "./index.html",
+    "./styles.css",
+    "./manifest.json"
 ];
 
 self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES))
-  );
-
-  self.skipWaiting();
-});
-
-self.addEventListener("activate", event => {
-  event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(
-        keys.map(key => {
-          if (key !== CACHE_NAME) {
-            return caches.delete(key);
-          }
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(cache => {
+            return cache.addAll(ARCHIVOS);
         })
-      )
-    )
-  );
-
-  self.clients.claim();
+    );
 });
 
 self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
-  );
+    event.respondWith(
+        caches.match(event.request).then(response => {
+            return response || fetch(event.request);
+        })
+    );
 });
