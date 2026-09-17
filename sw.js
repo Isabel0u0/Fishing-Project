eaec01db0e21eea5294b4b400a412edebe07535d
Isabel0u0 <anaisa2009.08@gmail.com>
2026-09-16 20:55:35 -0600

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,24 @@
+const CACHE_NAME = "phishguard-v1";
+
+const ARCHIVOS = [
+    "./",
+    "./index.html",
+    "./styles.css",
+    "./manifest.json"
+];
+
+self.addEventListener("install", event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(cache => {
+            return cache.addAll(ARCHIVOS);
+        })
+    );
+});
+
+self.addEventListener("fetch", event => {
+    event.respondWith(
+        caches.match(event.request).then(response => {
+            return response || fetch(event.request);
+        })
+    );
+});
