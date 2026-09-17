3e057733ab653bf8b99ba29a95f5580a74a54ffd
Isabel0u0 <anaisa2009.08@gmail.com>
2026-09-16 23:44:57 -0600

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,49 +1,39 @@
-const CACHE_NAME = "fishing-v4";
+const CACHE_NAME = "fishing-v5";
 
 const ARCHIVOS = [
-"./",
-"./index.html",
-"./styles.css",
-"./manifest.json",
-"./icon-192.png",
-"./icon-512.png"
+    "./",
+    "./index.html",
+    "./styles.css",
+    "./manifest.json",
+    "./icon-192.png",
+    "./icon-512.png"
 ];
 
 self.addEventListener("install", event => {
-self.skipWaiting();
-
-```
-event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-        return cache.addAll(ARCHIVOS);
-    })
-);
-```
-
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => cache.addAll(ARCHIVOS))
+            .then(() => self.skipWaiting())
+    );
 });
 
 self.addEventListener("activate", event => {
-event.waitUntil(
-caches.keys().then(keys =>
-Promise.all(
-keys.map(key => {
-if (key !== CACHE_NAME) {
-return caches.delete(key);
-}
-})
-)
-).then(() => self.clients.claim())
-);
+    event.waitUntil(
+        caches.keys().then(keys =>
+            Promise.all(
+                keys.map(key => {
+                    if (key !== CACHE_NAME) {
+                        return caches.delete(key);
+                    }
+                })
+            )
+        ).then(() => self.clients.claim())
+    );
 });
 
 self.addEventListener("fetch", event => {
-event.respondWith(
-fetch(event.request)
-.then(response => {
-return response;
-})
-.catch(() => {
-return caches.match(event.request);
-})
-);
+    event.respondWith(
+        fetch(event.request)
+            .catch(() => caches.match(event.request))
+    );
 });
